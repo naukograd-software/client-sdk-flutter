78d39e2b98b308a56a295f4c3798c222cc28e125
Timur Chernykh <t.chernykh@facct.ru>
2025-07-28 23:58:54 +0300

    added api to create audio options

--- lib/src/track/options.dart
+++ lib/src/track/options.dart
@@ -232,7 +232,9 @@ class CustomAudioCaptureOptions extends CustomTrackOptions {
   @override
   Map<String, dynamic> toMediaConstraintsMap() {
     final constaints = audioOptions.toMediaConstraintsMap();
-    return constaints;
+    final audioMap = <String, dynamic>{};
+    audioMap['audio'] = constaints;
+    return audioMap;
   }
 }
 
